8a09c20dd90f3d611a810ab99dc40f988154735f
raghavkolar <raghav.kolar@hotmail.com>
2019-03-14 20:30:07 +0000

    Java Primitive Data Types

---
+++ src/love/programming/MyPrimitiveTest.java
@@ -0,0 +1,54 @@
+package love.programming;
+
+public class MyPrimitiveTest {
+
+/* Byte
+
+ */
+    static byte mybyte;
+    /* Integer
+
+
+  */
+    static int myint;
+
+    static short myShort;
+
+    static long myLong;
+
+    static double myDouble;
+
+    static float myFloat;
+
+
+    /* Character
+
+     */
+    static String myString;
+
+    /*Boolean
+
+     */
+ static boolean myBoolean;
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Default Value of Byte is :" +mybyte);
+        System.out.println("Default Value of Integer is :" +myint);
+        System.out.println("Default Value of Short Integer is :" +myShort);
+        System.out.println("Default  LValue of Long is :" +myLong);
+        System.out.println("Default  Float Value e is :" +myFloat);
+        System.out.println("Default Double Value is :" +myDouble);
+        System.out.println("Default Value of String  is :" +myString);
+        System.out.println("Default Value of Boolean Type is :"+myBoolean);
+
+
+
+
+
+    }
+
+}
